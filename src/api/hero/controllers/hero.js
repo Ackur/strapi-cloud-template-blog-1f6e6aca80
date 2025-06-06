ac157fe6a7caf4b4830d48e0b96c44b94a6d6da5
vKremenok <vkremenok@lleida.net>
2025-06-06 23:48:44 +0200

    rebuild project

--- src/api/hero/controllers/hero.js
+++ src/api/hero/controllers/hero.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  about controller
+ * hero controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::hero.hero');
--- src/api/hero/routes/hero.js
+++ src/api/hero/routes/hero.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about router.
+ * hero router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about.about');
+module.exports = createCoreRouter('api::hero.hero');
